638fbaf70f6d6ebb3c70abefc945d70fb949d2f0
Amanda Cauzin <cauzinamanda@gmail.com>
2026-04-29 10:13:57 -0300

    if and else exercise

---
+++ aula 04 - condicoes/ex002.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+
+//condição
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Digija sempre usando cinto de segurança!`)
