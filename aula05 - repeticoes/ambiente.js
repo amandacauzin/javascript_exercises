ce60a772203603d412941156790351a148dec08e
Amanda Cauzin <cauzinamanda@gmail.com>
2026-05-06 21:25:56 -0300

    estruturas de repetição

---
+++ aula05 - repeticoes/ambiente.js
@@ -0,0 +1,16 @@
+//escrever 'Oi, tudo bem?' várias vezes
+
+var c = 1 //criando variavel de contador
+while (c <= 6){
+    console.log('Oi!Tudo bem?')
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//repetição com do while
+var c = 1
+do{
+    console.log('Oi! Tudo bem?')
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
