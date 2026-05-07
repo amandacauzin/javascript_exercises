b57c93e1686e88b2cb836e57e3878d249507d4c7
Amanda Cauzin <cauzinamanda@gmail.com>
2026-05-07 20:08:14 -0300

    update cond exercises

--- aula05 - repeticoes/ambiente.js
+++ aula05 - repeticoes/ambiente.js
@@ -14,3 +14,15 @@ do{
     console.log(`Passo ${c}`)
     c++
 } while (c <= 6)
+
+//exercicio de for
+for(var c=1; c <= 6; c++){
+    console.log(`Oi!Tudo bem?`)
+    console.log(`Passo ${c}`)
+}
+
+console.log(`Vai começar...`)
+for(var c = 1; c <= 4; c++){
+    console.log(c)
+}
+console.log('FIM')
